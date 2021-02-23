72e85e2962597fb2351fc65bcf0a6c81c3964f96
dulanjanabandara <dulanjanauop@gmail.com>
2021-02-23 08:46:28 +0530

    Added some css to the index.php

--- index.php
+++ index.php
@@ -5,8 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <style>
+        header {
+            background: gray;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <header>
+        <h1>Hello World!</h1>
+    </header>
 </body>
 </html>
